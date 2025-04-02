78b0590da5f0b2d40b94a3bb8f334b9fb86a2913
Christopher E. Siu <cesiu@calpoly.edu>
2025-04-02 10:56:51 -0700

    Added examples of C programs and Makefiles.

--- 04.02-make/hello.c
+++ 04.02-make/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+/* NOTE: The arguments to main are the command line arguments. In this case, we
+ *       don't actually need them, so we can list the parameters as "void"
+ *       instead to reassure the compiler that we're not just ignoring data. */
+int main(void) {
     printf("Hello, world!\n");
 
     return 0;
