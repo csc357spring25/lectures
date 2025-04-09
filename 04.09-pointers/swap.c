d3d58f02c663063d94a98c236494b9d579a7fd44
Christopher E. Siu <cesiu@calpoly.edu>
2025-04-09 11:04:05 -0700

    Added example of pointers.

--- 04.09-pointers/swap.c
+++ 04.09-pointers/swap.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-void swap(int, int);
+/* NOTE: To declare a function that takes two int pointers, returns nothing: */
+void swap(int *, int *);
 
 int main(void) {
     int z = 1, w = 2;
 
-    swap(z, w);
+    /* NOTE: To call a function that takes two int pointers: */
+    swap(&z, &w);
 
     printf("z (%p): %d\n", (void *)&z, z);
     printf("w (%p): %d\n", (void *)&w, w);
@@ -13,8 +15,15 @@ int main(void) {
     return 0;
 }
 
-void swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
+/* NOTE: In C, arguments are always pass-by-value, but the value of a pointer
+ *       is a reference. Passing a pointer emulates pass-by-reference behavior,
+ *       essentially telling a function *where* the arguments can be found
+ *       rather than than *what* the arguments are. */
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    printf("x (%p): %p\n", (void *)&x, (void *)x);
+    printf("y (%p): %p\n", (void *)&y, (void *)y);
 }
