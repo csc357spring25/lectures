8e3abad21cbea99786c7b1915de7febd39a130eb
Christopher Siu <christopher@siu.sh>
2025-04-18 10:45:14 -0700

    Added example of dynamic allocation.

--- 04.18-malloc/pair.c
+++ 04.18-malloc/pair.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int *pair(int, int);
 
@@ -15,8 +16,19 @@ int main(void) {
 }
 
 int *pair(int first, int second) {
-    int arr[2];
+    /* NOTE: Returning this array is never safe; the array is local, allocated
+     *       on the runtime stack, and it will be deallocated as soon as the
+     *       function returns, so its address is effectively garbage:
+     * int arr[2]; */
 
+    /* NOTE: By instead dynamically allocating space on the heap, we don't need
+     *       to know how much space we need at compile-time, and we don't need
+     *       to worry that the space will be deallocated on return: */
+    int *arr = (int *)malloc(sizeof(int) * 2);
+
+    /* NOTE: Since array and pointer syntax is interchangeable, the compiler
+     *       will then allow us to index the pointer to dynamically allocated
+     *       memory as though it were a true (local) array: */
     arr[0] = first;
     arr[1] = second;
 
