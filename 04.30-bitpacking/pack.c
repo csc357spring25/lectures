83730249af868da19fbf77e2c7cfe5663feffe20
Christopher E. Siu <cesiu@calpoly.edu>
2025-04-30 09:12:04 -0700

    Added base code for bitpacking and error checking.

---
+++ 04.30-bitpacking/pack.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+unsigned char stob(char *);
+
+int main(int argc, char* argv[]) {
+    unsigned char byte;
+    char buf[8];
+    int src, dest;
+
+    src = open(argv[1], O_RDONLY);
+    dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    while (read(src, buf, sizeof(char) * 8) > 0) {
+        byte = stob(buf);
+        write(dest, &byte, sizeof(char) * 1);
+    }
+
+    close(src);
+    close(dest);
+
+    return 0;
+}
+
+unsigned char stob(char *bits) {
+    return 0;
+}
