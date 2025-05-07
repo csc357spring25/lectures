5275ebcf8dda9860852264bd1c37baa7810a08b4
cesiu <cesiu@calpoly.edu>
2025-05-07 11:03:22 -0700

    Added example of basic parallelization using fork.

--- 05.07-fork/search.c
+++ 05.07-fork/search.c
@@ -1,3 +1,6 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,9 +10,35 @@
 int fsearch(char *, char *);
 
 int main(int argc, char *argv[]) {
-    int status = EXIT_FAILURE;
+    int i, status = EXIT_FAILURE, tmp;
+    pid_t pid;
 
-    status = fsearch(argv[2], argv[1]);
+    for (i = 2; i < argc; i++) {
+        if ((pid = fork()) == 0) {
+            printf("%ld is the child of %ld.\n", (long)getpid(), (long)getppid());
+            status = fsearch(argv[i], argv[1]);
+            exit(status);
+        }
+        else {
+            printf("%ld is the parent of %ld.\n", (long)getpid(), (long)pid);
+
+            /* NOTE: Parents should eventually wait for their children, but we
+             *       can't do that here, otherwise the second child will not be
+             *       created until after the first child terminates, which
+             *       defeats the point of parallelizing using processes. */
+        }
+    }
+
+    for (i = 2; i < argc; i++) {
+        pid = wait(&tmp);
+        if (WIFEXITED(tmp)) {
+            printf("%ld exited with status %d.\n", (long)pid, WEXITSTATUS(tmp));
+            status = status && WEXITSTATUS(tmp);
+        }
+        else {
+            printf("%ld exited abnormally.\n", (long)pid);
+        }
+    }
 
     return status;
 }
