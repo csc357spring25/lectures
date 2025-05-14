44031cc3be37260c6e46185c80da25da6dc9a98f
Christopher E. Siu <cesiu@calpoly.edu>
2025-05-14 07:55:08 -0700

    Added base code for timers.

---
+++ 05.14-timers/sporkbomb.c
@@ -0,0 +1,34 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "spork.h"
+
+int main(void) {
+    int status;
+    pid_t pid;
+
+    if ((pid = spork(5)) == 0) {
+        printf("%ld is the child of %ld.\n", (long)getpid(), (long)getppid());
+
+        /*
+        while (1) {
+            fork();
+            perror("fork");
+            sleep(1);
+        }
+        */
+    }
+    else {
+        printf("%ld is the parent of %ld.\n", (long)getpid(), (long)pid);
+        pid = wait(&status);
+
+        if (WIFEXITED(status)) {
+            printf("%ld exited status %d.\n", (long)pid, WEXITSTATUS(status));
+        }
+        else {
+            printf("%ld terminated abnormally.\n", (long)pid);
+        }
+    }
+
+    return 0;
+}
