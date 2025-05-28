d50ca1cc0b91b5d5edca296dde41f118a599ab74
cesiu <cesiu@calpoly.edu>
2025-05-28 10:55:07 -0700

    Added examples of sockets.

--- 05.28-sockets/client.c
+++ 05.28-sockets/client.c
@@ -1,11 +1,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints = {0}, *addr;
+    int fd, i = 0;
+    char buf[15] = "Hello, server!";
 
     /* NOTE: This program, the client, will actively attempt to connect to the
      *       server, which requires knowing the server's address and port. */
@@ -13,8 +16,22 @@ int main(int argc, char *argv[]) {
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
+    /* NOTE: For simplicity, we'll assume that the first address will always
+     *       work instead of iterating over the list of addresses. */
+
     getaddrinfo(argv[1], argv[2], &hints, &addr);
+    fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    /* NOTE: It is possible that the entire buffer cannot be written at once,
+     *       in which case we have to pick up where we left off. */
+
+    connect(fd, addr->ai_addr, addr->ai_addrlen);
+    while (i < 14) {
+        i += write(fd, buf + i, 14 - i);
+    }
+
     freeaddrinfo(addr);
+    close(fd);
 
     return EXIT_SUCCESS;
 }
--- 05.28-sockets/server.c
+++ 05.28-sockets/server.c
@@ -1,11 +1,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints = {0}, *addr;
+    int fd, client, n;
+    char buf[5];
 
     /* NOTE: This program, the server, will passively run on one computer and
      *       wait for other programs, the clients, to establish connections. */
@@ -14,8 +17,35 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    /* NOTE: For simplicity, we'll assume that the first address will always
+     *       work instead of iterating over the list of addresses. */
+
     getaddrinfo(NULL, argv[1], &hints, &addr);
+    fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    /* NOTE: The server must bind its socket to a specific port, on which it
+     *       will then listen for new incoming connections. */
+
+    bind(fd, addr->ai_addr, addr->ai_addrlen);
+    listen(fd, 1);
+
+    /* NOTE: If we cared to know the client's address, we could pass pointers
+     *       to structures which would be populated by accept. */
+
+    client = accept(fd, NULL, NULL);
+
+    /* NOTE: This creates a new socket for communication with the accepted
+     *       client; the existing socket can continue to be used to listen for
+     *       new incoming connections. */
+
+    while ((n = read(client, buf, 4)) > 0) {
+        buf[n] = '\0';
+        printf("Client sent \"%s\".\n", buf);
+    }
+
     freeaddrinfo(addr);
+    close(client);
+    close(fd);
 
     return EXIT_SUCCESS;
 }
